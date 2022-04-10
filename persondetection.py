24f6c0ca782dacc70e3d0edeebd8144264993037
Nikhil <nikhilpal9111@gmail.com>
2022-04-10 15:29:47 +0530

    commit

--- persondetection.py
+++ persondetection.py
@@ -1,13 +1,9 @@
 import numpy as np
 import tensorflow as tf
-# import cv2
 import time
 import os
-# import tensorflow.compat.v1 as tf
 import tensorflow._api.v2.compat.v1 as tf
 tf.disable_v2_behavior()
-
-
 class DetectorAPI:
     def __init__(self):
         path = os.path.dirname(os.path.realpath(__file__))
@@ -24,20 +20,19 @@ class DetectorAPI:
         self.default_graph = self.detection_graph.as_default()
         self.sess = tf.Session(graph=self.detection_graph)
 
-        # Definite input and output Tensors for detection_graph
+  
         self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-        # Each box represents a part of the image where a particular object was detected.
+       
         self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
+      
         self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
         self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
         self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
     def processFrame(self, image):
-        # Expand dimensions since the trained_model expects images to have shape: [1, None, None, 3]
+      
         image_np_expanded = np.expand_dims(image, axis=0)
-        # Actual detection.
+        
         start_time = time.time()
         (boxes, scores, classes, num) = self.sess.run(
             [self.detection_boxes, self.detection_scores,
@@ -45,8 +40,7 @@ class DetectorAPI:
             feed_dict={self.image_tensor: image_np_expanded})
         end_time = time.time()
 
-        # print("Elapsed Time:", end_time-start_time)
-        # print(self.image_tensor, image_np_expanded)
+
         im_height, im_width, _ = image.shape
         boxes_list = [None for i in range(boxes.shape[1])]
 
